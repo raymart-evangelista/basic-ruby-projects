406a96eb63f671f51cf8113d66bcc568e92d7e54
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-04-11 12:38:12 -0700

    revise logic to scan for multiple instances instead of returning first instance

--- sub-strings.rb
+++ sub-strings.rb
@@ -5,12 +5,16 @@ def substrings(string, dictionary)
   # use the dictionary to match words that can be found in the word
   dictionary.select do |word|
     if string.match?(word)
-      # if the matched word is already hashed, add 1 to the count else we start a new count
-      if found_substrings.has_key?(word)
-        found_substrings[word] += 1
-      else
-        found_substrings[word] = 1
-      end
+
+      # scan for how many instances the word pops up in the string and put the number of instances in the has
+      found_substrings[word] = string.scan(word).size
+      
+      # # if the matched word is already hashed, add 1 to the count else we start a new count
+      # if found_substrings.has_key?(word)
+      #   found_substrings[word] += 1
+      # else
+      #   found_substrings[word] = 1
+      # end
     end
   end
   # return found_substrings hash
@@ -24,3 +28,4 @@ p substrings("below", dictionary)
 # should return { "below" => 1, "low" =>}
 
 p substrings("Howdy partner, sit down! How's it going?", dictionary)
+# should return { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
